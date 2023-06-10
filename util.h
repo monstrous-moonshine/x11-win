95642ab62ff1545822613866082e751ce74bcf99
Khalid Bin Walid <kbwalid26@gmail.com>
2023-06-10 17:03:58 +0900

    read full server response on connection

--- util.h
+++ util.h
@@ -3,6 +3,52 @@
 
 #include <stdint.h>
 
+struct pixmap_format {
+    uint8_t depth;
+    uint8_t bpp;
+    uint8_t scanline_pad;
+    uint8_t padding[5];
+} __attribute__((packed));
+
+struct x_visual {
+    uint32_t visual_id;
+    uint8_t visual_class;
+    uint8_t bit_per_rgb_value;
+    uint16_t colormap_entries;
+    uint32_t red_mask;
+    uint32_t green_mask;
+    uint32_t blue_mask;
+    uint8_t pad_1[4];
+} __attribute__((packed));
+
+struct x_depth {
+    uint8_t depth;
+    uint8_t pad_1[1];
+    uint16_t num_visual;
+    uint8_t pad_2[4];
+    struct x_visual *visuals;
+} __attribute__((packed));
+
+struct x_screen {
+    uint32_t root;
+    uint32_t default_colormap;
+    uint32_t white_pixel;
+    uint32_t black_pixel;
+    uint32_t current_input_masks;
+    uint16_t width_px;
+    uint16_t height_px;
+    uint16_t width_mm;
+    uint16_t height_mm;
+    uint16_t min_installed_map;
+    uint16_t max_installed_map;
+    uint32_t root_visual;
+    uint8_t  backing_stores;
+    uint8_t  save_unders;
+    uint8_t  root_depth;
+    uint8_t  num_allowed_depth;
+    struct x_depth *allowed_depths;
+} __attribute__((packed));
+
 struct x_header {
     uint8_t status;
     uint8_t pad_1[1];
@@ -29,10 +75,17 @@ struct x_header {
             uint8_t min_keycode;
             uint8_t max_keycode;
             uint8_t pad_1[4];
+            char *vendor;
+            struct pixmap_format *pixmap_formats;
+            struct x_screen *roots;
         } pass;
     };
 } __attribute__((packed));
 
-static_assert(sizeof(struct x_header) == 40, "wrong size for header");
+static_assert(sizeof(struct x_header) == 64, "wrong size for header");
+static_assert(sizeof(struct x_screen) == 48, "wrong size for header");
+static_assert(sizeof(struct x_depth) == 16, "wrong size for header");
+static_assert(sizeof(struct x_visual) == 24, "wrong size for header");
+static_assert(sizeof(struct pixmap_format) == 8, "wrong size for header");
 
 #endif
--- xwin.c
+++ xwin.c
@@ -11,28 +11,77 @@
 #define X_CONNECTION_OKAY 1
 #define X_CONNECTION_AUTH 2
 
+/* from X11/Xauth.h */
+#define FamilyLocal 256
+
 void die(const char *msg) {
     perror(msg);
     exit(1);
 }
 
+void free_x_header(struct x_header *x_conn) {
+    if (x_conn->status != X_CONNECTION_OKAY) {
+        free(x_conn->fail.reason);
+    } else {
+        free(x_conn->pass.vendor);
+        free(x_conn->pass.pixmap_formats);
+        for (uint8_t i = 0; i < x_conn->pass.num_screen; i++) {
+            for (uint8_t j = 0; j < x_conn->pass.roots[i].num_allowed_depth; j++) {
+                free(x_conn->pass.roots[i].allowed_depths[j].visuals);
+            }
+            free(x_conn->pass.roots[i].allowed_depths);
+        }
+        free(x_conn->pass.roots);
+    }
+}
+
 uint16_t read_short(FILE *fp) {
     uint8_t buf[2];
-    if (fread(buf, 1, 2, fp) != 2)
-        die("fread");
+    if (fread(buf, 1, 2, fp) != 2) {
+        if (feof(fp)) {
+            fprintf(stderr, "ERROR: end of file\n");
+            exit(1);
+        } else {
+            die("fread");
+        }
+    }
     return (buf[0] << 8) | buf[1];
 }
 
-int read_magic_cookie(char *dest) {
+int read_magic_cookie(char *dest, int dest_size) {
     int ret = 0;
     char xauth_fname[256];
-    sprintf(xauth_fname, "%s/.Xauthority", getenv("HOME"));
+    snprintf(xauth_fname, 256, "%s/.Xauthority", getenv("HOME"));
     FILE *fp = fopen(xauth_fname, "rb");
     if (!fp) {
         fprintf(stderr, "ERROR: can't open Xauthority file\n");
         exit(1);
     }
-    while (!feof(fp)) {
+    fseek(fp, 0, SEEK_END);
+    ssize_t fsize = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    /* The Xauthority file contains entries of the following form:
+     *
+     * struct xauth_entry {
+     *   uint16_t family;
+     *   uint16_t addr_len;
+     *   char     addr[addr_len];
+     *   uint16_t num_len;
+     *   char     num[num_len];
+     *   uint16_t name_len;
+     *   char     name[name_len];
+     *   uint16_t data_len;
+     *   char     data[data_len];
+     * };
+     *
+     * Here, we only check if the family is FamilyLocal. We assume
+     * the name is "MIT-MAGIC-COOKIE-1", and ignore the address and
+     * the display number. If the family matches, we read the cookie
+     * data into the dest buffer. In future, we probably should at
+     * least check the protocol name too.
+     */
+    while (ftell(fp) < fsize) {
         uint16_t family = read_short(fp);
         uint16_t addr_len = read_short(fp);
         fseek(fp, addr_len, SEEK_CUR);
@@ -41,7 +90,8 @@ int read_magic_cookie(char *dest) {
         uint16_t name_len = read_short(fp);
         fseek(fp, name_len, SEEK_CUR);
         uint16_t data_len = read_short(fp);
-        if (family == 0x100) {
+        if (family == FamilyLocal) {
+            assert(data_len <= dest_size);
             fread(dest, 1, data_len, fp);
             ret = 1;
             break;
@@ -55,16 +105,17 @@ int read_magic_cookie(char *dest) {
 
 void x_connect(int xfd, struct x_header *x_conn) {
     struct {
-        char byte_order[2];
+        char byte_order;
+        char pad_1;
         uint16_t version_major;
         uint16_t version_minor;
         uint16_t len_auth_proto_name;
         uint16_t len_auth_proto_data;
-        char padding[2];
+        uint16_t pad_2;
         char auth_proto_name[20];
         char auth_proto_data[16];
     } __attribute__((packed)) header = {
-        .byte_order = { 'l' },
+        .byte_order = 'l',
         .version_major = 11,
         .version_minor = 0,
         .len_auth_proto_name = 18,
@@ -74,7 +125,7 @@ void x_connect(int xfd, struct x_header *x_conn) {
 
     static_assert(sizeof(header) == 48, "wrong size for header");
 
-    if (!read_magic_cookie(header.auth_proto_data)) {
+    if (!read_magic_cookie(header.auth_proto_data, 16)) {
         fprintf(stderr, "ERROR: can't read cookie from Xauthority file\n");
         exit(1);
     }
@@ -86,10 +137,56 @@ void x_connect(int xfd, struct x_header *x_conn) {
         die("read");
 
     if (x_conn->status != X_CONNECTION_OKAY) {
-        x_conn->fail.reason = malloc(x_conn->length * 4);
+        if (!(x_conn->fail.reason = malloc(x_conn->length * 4)))
+            die("malloc");
         if (read(xfd, x_conn->fail.reason, x_conn->length * 4) != x_conn->length * 4)
             die("read");
         return;
+    }
+
+    if (read(xfd, &x_conn->pass, 32) != 32)
+        die("read");
+
+    int vendor_len = (x_conn->pass.vendor_len + 3) / 4 * 4;
+    if (!(x_conn->pass.vendor = malloc(vendor_len)))
+        die("malloc");
+
+    if (read(xfd, x_conn->pass.vendor, vendor_len) != vendor_len)
+        die("read");
+
+    int px_fmt_len = x_conn->pass.num_pixmap_format * sizeof(struct pixmap_format);
+    if (!(x_conn->pass.pixmap_formats = malloc(px_fmt_len)))
+        die("malloc");
+
+    if (read(xfd, x_conn->pass.pixmap_formats, px_fmt_len) != px_fmt_len)
+        die("read");
+
+    int screen_len = x_conn->pass.num_screen * sizeof(struct x_screen);
+    if (!(x_conn->pass.roots = malloc(screen_len)))
+        die("malloc");
+
+    for (uint8_t i = 0; i < x_conn->pass.num_screen; i++) {
+        if (read(xfd, &x_conn->pass.roots[i], sizeof(struct x_screen) - 8)
+                != sizeof(struct x_screen) - 8)
+            die("read");
+
+        int allowed_depths_len = x_conn->pass.roots[i].num_allowed_depth
+            * sizeof(struct x_depth);
+        if (!(x_conn->pass.roots[i].allowed_depths = malloc(allowed_depths_len)))
+            die("malloc");
+
+        for (uint8_t j = 0; j < x_conn->pass.roots[i].num_allowed_depth; j++) {
+            if (read(xfd, &x_conn->pass.roots[i].allowed_depths[j], sizeof(struct x_depth) - 8)
+                    != sizeof(struct x_depth) - 8)
+                die("read");
+            int visual_len = x_conn->pass.roots[i].allowed_depths[j].num_visual
+                * sizeof(struct x_visual);
+            if (!(x_conn->pass.roots[i].allowed_depths[j].visuals = malloc(visual_len)))
+                die("malloc");
+
+            if (read(xfd, x_conn->pass.roots[i].allowed_depths[j].visuals, visual_len) != visual_len)
+                die("read");
+        }
     }
 }
 
@@ -107,7 +204,10 @@ int main() {
     x_connect(xfd, &x_conn);
     if (x_conn.status != X_CONNECTION_OKAY) {
         fprintf(stderr, "ERROR: can't connect to X server (%d): %s\n", x_conn.status, x_conn.fail.reason);
-        free(x_conn.fail.reason);
+        free_x_header(&x_conn);
         exit(1);
     }
+
+    printf("vendor: %.*s\n", x_conn.pass.vendor_len, x_conn.pass.vendor);
+    free_x_header(&x_conn);
 }
